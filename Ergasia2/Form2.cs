89b28f5c6ff6165b477cf2989ce9915a8ff0346f
Tina <ssaouach@harborlab.com>
2023-01-24 17:01:48 +0200

    Add project files.

---
+++ Ergasia2/Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ergasia2
+{
+    public partial class Form2 : Form
+    {
+        int scor = 0;
+        Random rand = new Random();
+        List<PictureBox> list = new List<PictureBox>();
+        private object items;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            PictureBox temPic = sender as PictureBox;
+            items.Remove(temPic);
+            this.Controls.Remove(temPic);
+            }
+    }
+}
